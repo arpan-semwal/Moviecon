d0f6df99fdff9ae694e0960702a4a346ead9aceb
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2023-06-10 10:33:22 +0530

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,18 +7,25 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
+// import logo from "../../assets/popcorn.png";
 
 const Header = () => {
-    const [show, setShow] = useState("top");
+    const [show, setShow] = useState("top"); // creating scrolling effect of menu ,  default value is top . 
     const [lastScrollY, setLastScrollY] = useState(0);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [query, setQuery] = useState("");
-    const [showSearch, setShowSearch] = useState("");
-    const navigate = useNavigate();
+    const [mobileMenu, setMobileMenu] = useState(false);// menu opening in mobile devices
+    const [query, setQuery] = useState(""); //text
+    const [showSearch, setShowSearch] = useState(""); // to show search bar
+    const navigate = useNavigate(); // navigate 
     const location = useLocation();
 
     return (
-        <div>Header</div>
+        <Header>
+            <ContentWrapper>
+                <div className="logo">
+                    {/* <img src={logo} alt = ""/> */}
+                </div>
+            </ContentWrapper>
+        </Header>
     );
 };
 
