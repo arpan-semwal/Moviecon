c6526f3965b5b616b76590cce6976df2a16cf673
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2023-06-15 22:34:13 +0530

    work

---
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import "./style.scss";
+
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import useFetch from "../../../hooks/useFetch";
+import Genres from "../../../components/genres/Genres";
+import CircleRating from "../../../components/circleRating/CircleRating";
+
+import PosterFallback from "../../../assets/poster1.jpg"
+import Img from "../../../components/lazyLoadImage/img";
+
+const DetailsBanner = ({ video, crew }) => {
+
+    const {mediaType , id} = useParams();
+    const {data , loading} = useFetch(`/${mediaType}/${id}`);
+//function to see the duration of the movie
+    const toHoursAndMinutes = (totalMinutes) => {
+        const hours = Math.floor(totalMinutes / 60);
+        const minutes = totalMinutes % 60;
+        return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+    };
+
+    
+    return (
+        <div className="detailsBanner">
+            {!loading ? (
+                <div>Details Content...</div>
+            ) : (
+                <div className="detailsBannerSkeleton">
+                    <ContentWrapper>
+                        <div className="left skeleton"></div>
+                        <div className="right">
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                            <div className="row skeleton"></div>
+                        </div>
+                    </ContentWrapper>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default DetailsBanner;
