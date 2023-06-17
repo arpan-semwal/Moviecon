f8a3f3b172e284e71bb9827dafa2eb9c6c5e0e8d
Arpan semwal <68865320+arpan-semwal@users.noreply.github.com>
2023-06-17 21:51:19 +0530

    details

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -12,11 +12,16 @@ import CircleRating from "../../../components/circleRating/CircleRating";
 
 import PosterFallback from "../../../assets/poster1.jpg"
 import Img from "../../../components/lazyLoadImage/img";
-
+import {PlayIcon} from "../Playbtn"
 const DetailsBanner = ({ video, crew }) => {
 
     const {mediaType , id} = useParams();
     const {data , loading} = useFetch(`/${mediaType}/${id}`);
+    const {url} = useSelector((state) => state.home);
+
+    // const _genres = data.genres.map((g) => g.id);
+
+
 //function to see the duration of the movie
     const toHoursAndMinutes = (totalMinutes) => {
         const hours = Math.floor(totalMinutes / 60);
@@ -28,12 +33,52 @@ const DetailsBanner = ({ video, crew }) => {
     return (
         <div className="detailsBanner">
             {!loading ? (
-                <div> 
-                    {/* <div className="backdrop-img">
-                        <Img src={URL.backdrop+data.backdrop_path}/>
-                    </div> */}
-                    detsils
-                </div>
+                <> 
+                  {!!data && (//!! boolean
+                    <React.Fragment>
+                        <div className="backdrop-img">
+                            <Img src={url.backdrop + data.backdrop_path}/>
+                        </div>
+                        <div className="opacity-layer"></div>
+                        <ContentWrapper>
+                            <div className="content">
+                                <div className="left">
+                                    {data.poster_path? (
+                                        <Img 
+                                        className="posterImg"
+                                        src={url.backdrop+data.poster_path}
+                                        />
+                                    ): (
+                                        <Img
+                                        className="posterImg"
+                                        src={PosterFallback}/>
+                                    )}
+                                </div>
+                                <div className="right">
+                                    <div className="title">
+                                        {`${data.name || data.title} (${dayjs(
+                                            data?.release_date).format("YYYY")})`}
+                                    </div>
+                                
+                                <div className="subtitle">
+                                    {data.tagline}
+                                </div>
+                            <div className="row">
+                                <CircleRating rating={data.vote_average.toFixed(1)}/>
+                                <div className="playbtn" onClick={() => {}}>
+                                  
+                                <PlayIcon/>
+                                <span className="text">Watch Trailer</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        </ContentWrapper>
+                    </React.Fragment>
+                  )
+                    
+                  }
+                </>
             ) : (
                 <div className="detailsBannerSkeleton">
                     <ContentWrapper>
